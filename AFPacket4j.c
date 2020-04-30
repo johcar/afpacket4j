bf5110ea3fa5120e5a50fa7705df777c89baa18c
johcar <johancarlstrom@gmail.com>
2020-04-30 15:12:25 +0200

    some cleanup in jni code

--- AFPacket4j.c
+++ AFPacket4j.c
@@ -1,7 +1,6 @@
 #include <jni.h>        // JNI header provided by JDK
 #include <stdio.h>      // C Standard IO Header
 #include "AFPacket4j.h" // Generated
-
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -17,6 +16,8 @@
 #include <poll.h>
 #include <signal.h>
 #include <linux/ip.h>
+
+
 
 // 4194304 bytes
 unsigned int blocksize = 1 << 22;
@@ -40,24 +41,25 @@ struct block_desc
    struct tpacket_hdr_v1 h1;
 };
 
-// this is the callback to java
+// method is of the callback funtion in java
 static jmethodID packet_callback;
-// method signature
+// method signature of the callback function in java
 static char *signature_string = "(Ljava/nio/ByteBuffer;)V";
+
+// prototypes
+void walk_block(JNIEnv *, jobject, struct block_desc *, const int);
+void handle_packet(JNIEnv *, jobject, uint8_t *, unsigned int);
+int rx_ring_setup(struct ring *, int);
+int get_interface_number_by_device_name(int, const char *);
+
 JNIEXPORT jint JNICALL Java_AFPacket4j_init(JNIEnv *env, jobject thisObj)
 {
-   printf("Hello World!\n");
    jclass class = (*env)->GetObjectClass(env, thisObj);
-   // Init - One time to initialize the method id, (use an init() function)
    packet_callback = (*env)->GetMethodID(env, class, "handlePacket", signature_string);
    if (packet_callback == 0)
    {
-      printf("unable to find method id\n");
+      printf("unable to find method id of java callback function\n");
       return -1;
-   }
-   else
-   {
-      printf("method id is %d\n", packet_callback);
    }
    return 1;
 }
@@ -67,7 +69,7 @@ JNIEXPORT jint JNICALL Java_AFPacket4j_socket(JNIEnv *env, jobject thisobj, jint
    int socket_fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
    if (socket_fd == -1)
    {
-      fprintf(stderr, "failed socket() call: %s\n", strerror(errno));
+      perror("socket() setup");
       return -1;
    }
 
@@ -88,7 +90,7 @@ JNIEXPORT jint JNICALL Java_AFPacket4j_bind(JNIEnv *env, jobject thisobj, jint s
    int bind_result = bind(socket, (struct sockaddr *)&bind_address, sizeof(bind_address));
    if (bind_result == -1)
    {
-      fprintf(stderr, "failed bind() call: %s\n", strerror(errno));
+      perror("bind() to interface");
       return -1;
    }
    return bind_result;
@@ -138,74 +140,43 @@ JNIEXPORT jint JNICALL Java_AFPacket4j_rx_1ring(JNIEnv *env, jobject thisobj, ji
    }
 }
 
+/**
+ * Walk one block of packets
+ */
 void walk_block(JNIEnv *env, jobject thisobj, struct block_desc *pbd, const int block_num)
 {
-   int num_pkts = pbd->h1.num_pkts, i;
-   unsigned long bytes = 0;
+   int num_pkts = pbd->h1.num_pkts;
    struct tpacket3_hdr *ppd;
 
-   ppd = (struct tpacket3_hdr *)((uint8_t *)pbd +
-                                 pbd->h1.offset_to_first_pkt);
-   for (i = 0; i < num_pkts; ++i)
+   ppd = (struct tpacket3_hdr *)((uint8_t *)pbd + pbd->h1.offset_to_first_pkt);
+   for (int i = 0; i < num_pkts; ++i)
    {
-      bytes += ppd->tp_snaplen;
-      //display(ppd);
-      handlePacket(env, thisobj, ((uint8_t *)ppd + ppd->tp_mac), ppd->tp_len); //bring JNIEnv etc here...
-      // TODO java callback using directallocated bytebuffer?
-
-      ppd = (struct tpacket3_hdr *)((uint8_t *)ppd +
-                                    ppd->tp_next_offset);
+      handle_packet(env, thisobj, ((uint8_t *)ppd + ppd->tp_mac), ppd->tp_len); 
+      ppd = (struct tpacket3_hdr *)((uint8_t *)ppd + ppd->tp_next_offset);
    }
-
-   //packets_total += num_pkts;
-   //bytes_total += bytes;
 }
 
-void handlePacket(JNIEnv *env, jobject o, uint8_t *buffer, unsigned int len)
+/**
+ * This calls our callback method with the packets content as a Direct Bytebuffer
+ */
+void handle_packet(JNIEnv *env, jobject o, uint8_t *buffer, unsigned int len)
 {
    jobject bytebuffer = (*env)->NewDirectByteBuffer(env, (void *)buffer, len);
    (*env)->CallVoidMethod(env, o, packet_callback, bytebuffer);
-}
+}   
 
-void display(struct tpacket3_hdr *ppd)
-{
-   struct ethhdr *eth = (struct ethhdr *)((uint8_t *)ppd + ppd->tp_mac);
-   struct iphdr *ip = (struct iphdr *)((uint8_t *)eth + ETH_HLEN);
-
-   if (eth->h_proto == htons(ETH_P_IP))
-   {
-      struct sockaddr_in ss, sd;
-      char sbuff[1025], dbuff[1025];
-
-      memset(&ss, 0, sizeof(ss));
-      ss.sin_family = PF_INET;
-      ss.sin_addr.s_addr = ip->saddr;
-      getnameinfo((struct sockaddr *)&ss, sizeof(ss),
-                  sbuff, sizeof(sbuff), NULL, 0, 1);
-
-      memset(&sd, 0, sizeof(sd));
-      sd.sin_family = PF_INET;
-      sd.sin_addr.s_addr = ip->daddr;
-      getnameinfo((struct sockaddr *)&sd, sizeof(sd),
-                  dbuff, sizeof(dbuff), NULL, 0, 1);
-
-      printf("%s -> %s, ", sbuff, dbuff);
-   }
-
-   printf("rxhash: 0x%x\n", ppd->hv1.tp_rxhash);
-}
-
+/**
+ * Setup ring, only supports TPACKET_V3 for now
+ */
 int rx_ring_setup(struct ring *ring, int socket_fd)
 {
-   printf("ring is set\n");
-   // set packet version
    int version = TPACKET_V3; // use the best version by default
    // TODO implement support for the other versions later
    int set_tpacket_opt_result = setsockopt(socket_fd, SOL_PACKET, PACKET_VERSION, &version, sizeof(version));
    if (set_tpacket_opt_result == -1)
    {
-      fprintf(stderr, "failed setsockopt() call: %s\n", strerror(errno));
-      return set_tpacket_opt_result;
+      perror("setsockopt() version");
+      return -1;
    }
 
    // setup tpacket req3
@@ -221,7 +192,8 @@ int rx_ring_setup(struct ring *ring, int socket_fd)
    int set_ring_opt_result = setsockopt(socket_fd, SOL_PACKET, PACKET_RX_RING, &ring->req, sizeof(ring->req));
    if (set_ring_opt_result == -1)
    {
-      fprintf(stderr, "failed setsockopt() call: %s\n", strerror(errno));
+      perror("setsockopt() set ring");
+      return -1;
    }
    ring->map = mmap(NULL, ring->req.tp_block_nr * ring->req.tp_block_size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_LOCKED, socket_fd, 0);
    if (ring->map == MAP_FAILED)
@@ -239,7 +211,9 @@ int rx_ring_setup(struct ring *ring, int socket_fd)
    return 1;
 }
 
-// Get interface number by name
+/**
+ *  Get interface number by its name
+ */
 int get_interface_number_by_device_name(int socket_fd, const char *interface)
 {
    struct ifreq ifr;
